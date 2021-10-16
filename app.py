03af67ec307d74eb37f0e54c7522a963d3f09b75
soham1993 <38506847+soham1993@users.noreply.github.com>
2021-10-16 18:35:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -32,7 +32,6 @@ def get_productdetails(searchterm):
     url='https://www.amazon.in/s?k='+searchterm
     source = requests.get(url, headers = headers,timeout=1).text
     soup = BeautifulSoup(source, 'html.parser')
-    print(soup)
     Names = []
     #loop to extract the ur of the product 
     for i in soup.find_all('a', class_='a-link-normal a-text-normal'):
@@ -44,7 +43,7 @@ def get_productdetails(searchterm):
             pass
     Product_Title,price,Product_url=[],[],[]
     Names=Names[:min(15,len(Names))]
-   
+    print(Names)
     ##Extracting the title ,price of the product
     for i in Names:
         print(i)
