83089c0af87c4ba49c7b5446a4ce97fd359e8785
soham1993 <38506847+soham1993@users.noreply.github.com>
2021-10-16 18:48:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ import time
 def get_productdetails(searchterm):
     print('Fetching product list')
     searchterm='+'.join(searchterm.split())
-    headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 Safari/537.36'}
+    #headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 Safari/537.36'}
     url='https://www.amazon.in/s?k='+searchterm
     source = requests.get(url, headers = headers,timeout=1).text
     soup = BeautifulSoup(source, 'html.parser')
