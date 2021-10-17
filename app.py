3822b55fc57370a8746b7a1857b90b0f878041e7
soham1993 <38506847+soham1993@users.noreply.github.com>
2021-10-17 10:07:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -27,9 +27,10 @@ import time
 
 def get_productdetails(searchterm):
     print('Fetching product list')
+    headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 Safari/537.36'}
     searchterm='+'.join(searchterm.split())
     url='https://www.amazon.in/s?k='+searchterm
-    source = requests.get(url,timeout=10).text
+    source = requests.get(url,headers=headers,timeout=10).text
     soup = BeautifulSoup(source, 'html.parser')
     #print(soup)
     Names = []
@@ -48,7 +49,7 @@ def get_productdetails(searchterm):
     for i in Names:
         print(i)
         try:
-            source2 = requests.get(i).text
+            source2 = requests.get(i,headers=headers).text
             soup2 = BeautifulSoup(source2,'html.parser')
             Title = soup2.find('span', id='productTitle').text
             Title = Title.strip()
@@ -101,7 +102,7 @@ def get_productreviews(productlist):
     reviewlist = []
     product_url=list(productlist['Product_url'].values)
     def get_soup(url):
-        r = requests.get(url)
+        r = requests.get(url,headers=headers)
         soup = BeautifulSoup(r.text, 'html.parser')
         return soup
     def get_reviews(soup,asin):
